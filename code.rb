50b938b3e0d42393b66fcfe99d2c5218742004ee
krishnakeerthi <krishnakeerthi675@gmail.com>
2019-02-20 07:53:06 -0500

    Initial commit

---
+++ code.rb
@@ -0,0 +1,5 @@
+items_we_have = "chips popcorn skittles granola mentos gum cheetos m&ms"
+puts "Please enter the item of your choice below : "
+item_name = gets.chomp
+item_index=items_we_have.index(item_name)
+if
